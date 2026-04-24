d2d41e9652aa783231a70d297d8f0a62236ed4e9
Max Waldner <maxwaldner@Maxs-MacBook-Air.local>
2026-04-24 14:10:21 -0500

    added LED toggle

--- server.cpp
+++ server.cpp
@@ -14,44 +14,81 @@
 constexpr int PORT = 8080;
 constexpr int BUFFER_SIZE = 1024;
 
-
 int ledVal = 0;
 
 int turnOnLED() {
-
-    const char* chipName = "/dev/gpiochip0";
+    const char *chipName = "/dev/gpiochip0";
     const unsigned int gpioPin = 17;
-    gpiod_chip* chip = gpiod_chip_open(chipName);
+    gpiod_chip *chip = gpiod_chip_open(chipName);
 
-    if (chip == nullptr) {
-        std::cerr << "Failed to open GPIO\n";
+    if (chip == nullptr)
+    {
+        std::cerr << "Failed to open GPIO chip\n";
         return 1;
     }
 
-    gpiod_line* line = gpiod_chip_get_line(chip, gpioPin);
+    gpiod_line_settings *settings = gpiod_line_settings_new();
+    if (settings == nullptr) {
+        std::cerr << "Failed to create line settings\n";\
+        gpiod_chip_close(chip);
+        return 1;
+    }
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
 
-    if (line == nullptr) {
-        std::cerr << "Failed to get GPIO line\n";
+    if (ledVal == 1) {
+        gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_ACTIVE);
+    }
+    else {
+        gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_INACTIVE);
+    }
+
+    gpiod_line_config *lineConfig = gpiod_line_config_new();
+
+    if (lineConfig == nullptr) {
+        std::cerr << "Failed to create line config\n";
+        gpiod_line_settings_free(settings);
         gpiod_chip_close(chip);
         return 1;
     }
 
-    if (gpiod_line_request_output(line, "led-control", 0) < 0) {
-        std::cerr << "Failed to request GPIO line as output\n";
+    gpiod_line_config_add_line_settings(lineConfig, &gpioPin, 1, settings);
+    gpiod_request_config *requestConfig = gpiod_request_config_new();
+
+    if (requestConfig == nullptr){
+        std::cerr << "Failed to create request config\n";
+        gpiod_line_config_free(lineConfig);
+        gpiod_line_settings_free(settings);
+        gpiod_chip_close(chip); 
+        return 1;
+    }
+
+    gpiod_request_config_set_consumer(requestConfig, "led-control");
+
+    gpiod_line_request *request = gpiod_chip_request_lines(chip, requestConfig, lineConfig);
+
+    if (request == nullptr) {
+        std::cerr << "Failed to request GPIO line\n";
+        gpiod_request_config_free(requestConfig);
+        gpiod_line_config_free(lineConfig);
+        gpiod_line_settings_free(settings);
         gpiod_chip_close(chip);
         return 1;
     }
-    gpiod_line_set_value(line, ledVal);
+
+    std::cout << "LED value set to: " << ledVal << std::endl;
     ledVal++;
     ledVal = ledVal % 2;
-    gpiod_line_release(line);
+    
+    gpiod_line_request_release(request);
+    gpiod_request_config_free(requestConfig);
+    gpiod_line_config_free(lineConfig);
+    gpiod_line_settings_free(settings);
     gpiod_chip_close(chip);
     return 0;
 }
 
-
-
-int tcp_server() {
+int tcp_server()
+{
 
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -59,17 +96,20 @@ int tcp_server() {
     socklen_t addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
         perror("setsockopt SO_REUSEADDR");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) < 0)
+    {
         perror("setsockopt SO_REUSEPORT");
         exit(EXIT_FAILURE);
     }
@@ -78,44 +118,43 @@ int tcp_server() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
     std::cout << "Server listening on port " << PORT << std::endl;
-    while (true) {
+    while (true)
+    {
 
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0 ) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
+        {
             perror("listen");
             exit(EXIT_FAILURE);
         }
-    
+
         ssize_t valread = read(new_socket, buffer, BUFFER_SIZE);
         std::cout << "Received: " << buffer << std::endl;
 
-        if (std::strcmp(buffer, "penis") == 0) {
+        if (std::strcmp(buffer, "penis") == 0)
+        {
             turnOnLED();
         }
         send(new_socket, buffer, valread, 0);
         std::cout << "echo message sent" << std::endl;
         close(new_socket);
-
     }
     close(server_fd);
     return 0;
 }
 
-
-
-
-
-
-
-int main() {
+int main()
+{
     tcp_server();
-} 
+}
